f69a0e21490c85b6af3e1c6059337a3a00bacbd1
Demur Nodia <demuri111@gmail.com>
2012-02-28 03:15:30 +0400

    DatabaseCopy !! @author demuri11

--- gen/ge/gtug/R.java
+++ gen/ge/gtug/R.java
@@ -14,10 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Help=0x7f060000;
-        public static final int Options=0x7f060001;
-        public static final int help=0x7f060003;
-        public static final int options=0x7f060002;
+        public static final int Help=0x7f070000;
+        public static final int Options=0x7f070004;
+        public static final int btnGo=0x7f070003;
+        public static final int help=0x7f070006;
+        public static final int linearLayout1=0x7f070002;
+        public static final int options=0x7f070005;
+        public static final int txtWord=0x7f070001;
     }
     public static final class layout {
         public static final int help=0x7f030000;
@@ -25,10 +28,21 @@ public final class R {
         public static final int options=0x7f030002;
     }
     public static final class menu {
-        public static final int gtug_menu=0x7f050000;
+        public static final int gtug_menu=0x7f060000;
+    }
+    public static final class raw {
+        public static final int enggeoaa=0x7f040000;
+        public static final int enggeoab=0x7f040001;
+        public static final int enggeoac=0x7f040002;
+        public static final int enggeoad=0x7f040003;
+        public static final int enggeoae=0x7f040004;
+        public static final int enggeoaf=0x7f040005;
+        public static final int enggeoag=0x7f040006;
+        public static final int enggeoah=0x7f040007;
+        public static final int enggeoai=0x7f040008;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- src/ge/gtug/GtugDictionaryActivity.java
+++ src/ge/gtug/GtugDictionaryActivity.java
@@ -1,43 +1,76 @@
 package ge.gtug;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 //Testing Commit
 
 public class GtugDictionaryActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    }
-    public boolean onCreateOptionsMenu(Menu menu) {
+	/** Called when the activity is first created. */
+	DataBaseHelper myDbHelper;
+	Button btnGo;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		 btnGo = (Button) findViewById(R.id.btnGo);
+		 myDbHelper = new DataBaseHelper(this);
+	       
+		
+		try {
+			myDbHelper.createDataBase();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	btnGo.setOnClickListener(new OnClickListener() {
+		
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			
+			
+			
+		}
+	});	
+	}
+
+	
+	
+	
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
 		super.onCreateOptionsMenu(menu);
 		MenuInflater koba = getMenuInflater();
 		koba.inflate(R.menu.gtug_menu, menu);
-		return  true;
+		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 		case R.id.options:
-			startActivity(new Intent (this, Options.class));
-             return true;
+			startActivity(new Intent(this, Options.class));
+			return true;
 		case R.id.help:
 			startActivity(new Intent(this, Help.class));
 			return true;
-		default: 
+		default:
 			return super.onOptionsItemSelected(item);
-			
+
 		}
-	}   
+	}
 }
