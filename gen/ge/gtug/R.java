e87a22d19606f8d88e94492f1af215e1fc101255
Giorgi Tkeshelashvili <gtkesh@gtkesh-PC.(none)>
2012-02-28 16:15:29 +0400

    OptionsMenu + HelpActivity @author gtkesh

--- gen/ge/gtug/R.java
+++ gen/ge/gtug/R.java
@@ -12,21 +12,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int tbilisigtug=0x7f020001;
     }
     public static final class id {
-        public static final int Help=0x7f070000;
-        public static final int Options=0x7f070005;
-        public static final int btnGo=0x7f070003;
-        public static final int help=0x7f070007;
-        public static final int linearLayout1=0x7f070002;
-        public static final int options=0x7f070006;
-        public static final int txtResult=0x7f070004;
-        public static final int txtWord=0x7f070001;
+        public static final int btnGo=0x7f070007;
+        public static final int help=0x7f070009;
+        public static final int imageView1=0x7f070002;
+        public static final int linearLayout1=0x7f070006;
+        public static final int scrollView1=0x7f070000;
+        public static final int tableRow1=0x7f070001;
+        public static final int tableRow2=0x7f070003;
+        public static final int textView1=0x7f070004;
+        public static final int txtResult=0x7f070008;
+        public static final int txtWord=0x7f070005;
     }
     public static final class layout {
         public static final int help=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int options=0x7f030002;
     }
     public static final class menu {
         public static final int gtug_menu=0x7f060000;
@@ -43,6 +45,7 @@ public final class R {
         public static final int enggeoai=0x7f040008;
     }
     public static final class string {
+        public static final int aboutUS=0x7f050002;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
     }
