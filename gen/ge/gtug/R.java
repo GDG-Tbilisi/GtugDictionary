99efb4fe0ddabb8ace8bde938c3a80826b33c595
Demur Nodia <demuri111@gmail.com>
2012-03-04 09:16:49 +0400

    გამშვები ხატულა ჩამატებულია

--- gen/ge/gtug/R.java
+++ gen/ge/gtug/R.java
@@ -15,9 +15,10 @@ public final class R {
         public static final int georgia=0x7f020001;
         public static final int gtugtbilisi=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int search=0x7f020005;
-        public static final int tbilisigtug=0x7f020006;
+        public static final int ic_launcherold=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int search=0x7f020006;
+        public static final int tbilisigtug=0x7f020007;
     }
     public static final class id {
         public static final int about=0x7f07000e;
